d919360ac070e170677772c95e38489ce1bba6cf
kervin5 <kervinlvh@gmail.com>
2017-11-12 17:40:08 -0800

    Complete pagination

--- includes/setup.php
+++ includes/setup.php
@@ -1,5 +1,6 @@
 <?php
 function ju_setup_theme(){
+    add_theme_support('post-thumbnails');
 //    add_theme_support('menus'); not required in newer versions of wordpress
 
     register_nav_menu('primary',__('Primary Menu', 'udemy'));
