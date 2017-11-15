8e804722fae89c2ab37ea35aba89a7bf9b03c094
kervin5 <kervinlvh@gmail.com>
2017-11-14 20:42:55 -0800

    Single post template

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                                         <?php if(has_post_thumbnail())  {?>
                                             <div class="entry-image">
                                                 <a href="<?php the_permalink(); ?>" data-lightbox="image">
-                                                    <?php the_post_thumbnail('full',['class' => 'image_face']); ?>
+                                                    <?php the_post_thumbnail('full',['class' => 'image_fade']); ?>
                                                 </a>
                                             </div>
                                         <?php } ?>
