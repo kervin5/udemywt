44ad88c137bf20ae1f92f2400d3131bd871efc3d
Kervin Vasquez <kervinlvh@gmail.com>
2018-05-10 22:46:24 -0700

    Enable extra features theme_support

--- includes/setup.php
+++ includes/setup.php
@@ -3,6 +3,10 @@ function ju_setup_theme(){
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('automatic-feed-links');//Enable RSS
+	add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption'));//Enable HTML5
+
+//	add_theme_support('starter-content',$starter_content);
 //    add_theme_support('menus'); not required in newer versions of wordpress
 
     register_nav_menu('primary',__('Primary Menu', 'udemy'));
