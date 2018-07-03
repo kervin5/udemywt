58b4599956192155c7c7761e876a6d4df6501c6b
Kervin Vasquez <kervinlvh@gmail.com>
2018-07-02 18:17:30 -0700

    Install buddypress and create basic templates

---
+++ buddypress.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+    <!-- Content
+	============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+
+            <div class="container clearfix">
+				<?php
+				while ( have_posts() ) {
+					the_post();
+
+					the_content();
+				}
+				?>
+            </div><!-- .postcontent end -->
+
+
+        </div>
+
+        </div>
+
+    </section><!-- #content end -->
+<?php get_footer(); ?>
