dd9b47d76e0eaa8faf8a267ecef861c582430e77
Kervin Vasquez <kervinlvh@gmail.com>
2018-07-04 20:24:45 -0700

    Custom buddypress CSS

--- functions.php
+++ functions.php
@@ -12,6 +12,10 @@ include (get_template_directory().'/includes/customizer/social.php');
 include (get_template_directory().'/includes/customizer/misc.php');
 include (get_template_directory().'/includes/customizer/enqueue.php');
 include (get_template_directory().'/includes/buddypress/cover-image.php');
+include (get_template_directory().'/includes/buddypress/profile-tabs.php');
+include (get_template_directory().'/includes/buddypress/profile-post.php');
+include (get_template_directory().'/includes/functions.php');
+
 //Hooks
 add_action('wp_enqueue_scripts','ju_enqueue');
 add_action('after_setup_theme','ju_setup_theme');
@@ -19,5 +23,5 @@ add_action('widgets_init','ju_widgets');
 add_action('customize_register','ju_customize_register');
 add_action('customize_preview_init','ju_customize_preview_init');
 add_filter('bp_before_xprofile_cover_image_settings_parse_args','ju_xprofile_cover_image');
-
+add_action('bp_setup_nav','ju_buddypress_profile_tabs');
 //Shortcodes
