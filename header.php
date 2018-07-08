7474fefb2a2ff86846abdd7cbd9ed38663153131
Kervin Vasquez <kervinlvh@gmail.com>
2018-07-08 16:48:07 -0700

    Feature: Implement shopping cart in theme's header

--- header.php
+++ header.php
@@ -129,36 +129,60 @@
                         <!-- Top Cart
 					 ============================================= -->
                         <div id="top-cart">
-                            <a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>5</span></a>
+                            <a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span><?php echo  WC()->cart->get_cart_contents_count();?></span></a>
                             <div class="top-cart-content">
                                 <div class="top-cart-title">
                                     <h4>Shopping Cart</h4>
                                 </div>
                                 <div class="top-cart-items">
-                                    <div class="top-cart-item clearfix">
-                                        <div class="top-cart-item-image">
-                                            <a href="#"><img src="images/shop/small/1.jpg" alt="Blue Round-Neck Tshirt" /></a>
-                                        </div>
-                                        <div class="top-cart-item-desc">
-                                            <a href="#">Blue Round-Neck Tshirt</a>
-                                            <span class="top-cart-item-price">$19.99</span>
-                                            <span class="top-cart-item-quantity">x 2</span>
-                                        </div>
-                                    </div>
-                                    <div class="top-cart-item clearfix">
-                                        <div class="top-cart-item-image">
-                                            <a href="#"><img src="images/shop/small/6.jpg" alt="Light Blue Denim Dress" /></a>
-                                        </div>
-                                        <div class="top-cart-item-desc">
-                                            <a href="#">Light Blue Denim Dress</a>
-                                            <span class="top-cart-item-price">$24.99</span>
-                                            <span class="top-cart-item-quantity">x 3</span>
-                                        </div>
-                                    </div>
+	                                <?php
+	                                foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+		                                $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+		                                $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+		                                if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+			                                $product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+			                                ?>
+                                            <div class="top-cart-item clearfix">
+                                                <div class="top-cart-item-image">
+	                                                <?php
+	                                                $thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+	                                                if ( ! $product_permalink ) {
+		                                                echo wp_kses_post( $thumbnail );
+	                                                } else {
+		                                                printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), wp_kses_post( $thumbnail ) );
+	                                                }
+	                                                ?>
+                                                </div>
+                                                <div class="top-cart-item-desc">
+                                                    <a href="#">
+	                                                    <?php
+	                                                    if ( ! $product_permalink ) {
+		                                                    echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+	                                                    } else {
+		                                                    echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+	                                                    }
+	                                                    ?>
+                                                    </a>
+                                                    <span class="top-cart-item-price">
+                                                         <?php
+                                                         echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+                                                         ?>
+                                                    </span>
+                                                    <span class="top-cart-item-quantity">x <?php echo $cart_item['quantity'];?></span>
+                                                </div>
+                                            </div>
+			                                <?php
+		                                }
+	                                }
+	                                ?>
+
+
                                 </div>
                                 <div class="top-cart-action clearfix">
-                                    <span class="fleft top-checkout-price">$114.95</span>
-                                    <button class="button button-3d button-small nomargin fright">View Cart</button>
+                                    <span class="fleft top-checkout-price"><?php echo WC()->cart->get_cart_contents_total();?></span>
+                                    <a href="<?php wp_get_cart_url();?>" class="button button-3d button-small nomargin fright">View Cart</a>
                                 </div>
                             </div>
                         </div><!-- #top-cart end -->
